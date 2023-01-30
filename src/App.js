02e1dcc18dec6eb4e6293cebcec51b19da6cba25
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-01-30 11:57:36 -0300

    Faltando apenas refatorar os componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
+import { toBeDisabled } from '@testing-library/jest-dom/dist/matchers';
+import React, { useState } from 'react';
+import { MdDelete } from "react-icons/md";
 import './App.css';
 
-function App() {
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const erase = () => {
+    setValue("");
+  }
+
+  const submit = () => {
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      }
+    ])
+    erase()
+  }
+
+  const onToggle = (todo) => {
+
+    setTodos(todos.map((obj) => (
+      obj.id === todo.id
+        ? {...obj, checked: !todo.checked}
+        : obj
+    )))
+  }
+
+  const deleteTask = (todo) => {
+
+   setTodos(todos.filter((obj) => obj.id !== todo.id))
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+    console.log(value)
+  }
+
+  const onKeyDown = (e) => {
+     if (e.which === ENTER_KEY) {
+      submit()
+     } else if (e.which === ESCAPE_KEY) {
+      erase()
+     }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section id="app" className='container'>
+      <header>
+        <h1 className='title'>Lista de tarefas</h1>
       </header>
-    </div>
+        <section className='main'>
+          <input
+              className='new-todo'
+              placeholder='o que precisa ser feito?'
+              value={value}
+              onChange={onChange}
+              onKeyDown={onKeyDown}
+          />
+          <ul className='todo-list'>
+              {
+                todos.map((todo) => (
+                  <li key={todo.id.toString()} >
+                    <span
+                      className={['todo', todo.checked ? 'checked' : ""].join(' ')}
+                      onClick={() => onToggle(todo)}
+                      onKeyDown={() => onToggle(todo)}
+                      role="button"
+                      tabIndex={0}
+                    >{todo.title}</span>
+                    <button className='remove'
+                            type='button'
+                            onClick={() => deleteTask(todo)} >
+                      <MdDelete size={28} />
+                    </button>
+                  </li>
+
+                ))
+              }
+          </ul>
+        </section>
+    </section>
   );
 }
 
